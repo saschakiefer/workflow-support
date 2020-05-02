c20d1c5ebb56b02ed52dffaf0e1a8775cc76d09d
Sascha Kiefer <mail@sascha-kiefer.de>
2020-05-02 10:29:43 +0200

    Update python path

--- scripts/fritz_status.py
+++ scripts/fritz_status.py
@@ -1,10 +1,12 @@
-#!/usr/local/bin/python3
+#!/usr/local/opt/python@3.8/bin/python3
 
 import time
+
 from fritzconnection.lib.fritzstatus import FritzStatus
 
-fc = FritzStatus(address='192.168.178.1')
+fc = FritzStatus(address="192.168.178.1")
 while True:
     print(
-        f"Linked: {fc.is_connected} -- Connected: {fc.is_linked} -- max bit rate: {fc.str_max_bit_rate} -- trans rate: {fc.str_transmission_rate}")
+        f"Linked: {fc.is_connected} -- Connected: {fc.is_linked} -- max bit rate: {fc.str_max_bit_rate} -- trans rate: {fc.str_transmission_rate}"
+    )
     time.sleep(2)
